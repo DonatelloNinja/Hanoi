54af6a37213f52046a833d6c33f9943cf2fa18d5
eanegwo <aneta.gwozdz@ericsson.com>
2017-11-10 15:40:18 +0100

    Initial commit

---
+++ Hanoi/MyStack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hanoi
+{
+    public class MyStack
+    {
+        public const int DEFAULT_STACK_SIZE = 10;
+
+        private int currentIndex = 0;
+        private int[] stack;
+
+        public MyStack(int size = DEFAULT_STACK_SIZE)
+        {
+            stack = new int[size];
+        }
+
+        public int Size()
+        {
+            return currentIndex;
+        }
+
+        public void Push(int element)
+        {
+            stack[currentIndex] = element;
+            currentIndex++;
+        }
+
+        public int Pop()
+        {
+            currentIndex--;
+            return stack[currentIndex];
+        }
+
+        public int GetMaxStackSize()
+        {
+            return stack.Count();
+        }
+
+        public void Clear()
+        {
+            currentIndex = 0;
+        }
+
+        public bool IsEmpty()
+        {
+            return currentIndex == 0;            
+        }
+    }
+}
---
+++ HanoiTests/StackTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Hanoi;
+
+namespace HanoiTests
+{
+    public class StackTest
+    {
+        public const int TEST_ELEMENT = 5;
+        [Test]
+        public void DefaultConstructorShouldSetStackSizeToDefaultValue()
+        {
+            var stack = new MyStack();
+            Assert.AreEqual(MyStack.DEFAULT_STACK_SIZE, stack.GetMaxStackSize());
+        }
+
+        [Test]
+        public void DefaultConstructorShouldSetStackSizeToParameterValue()
+        {
+            int wantedStackSize = 5;
+            var stack = new MyStack(wantedStackSize);
+            Assert.AreEqual(wantedStackSize, stack.GetMaxStackSize());
+        }
+
+        [Test]
+        public void StackSizeShouldBeZeroWhenCreated()
+        {
+            var stack = new MyStack();
+            Assert.Zero(stack.Size());
+        }
+
+        [Test]
+        public void StackSizeShouldBeIncrementedWhenItemAdded()
+        {
+            var stack = new MyStack();
+            stack.Push(TEST_ELEMENT);
+
+            int expectedStackSize = 1;
+            Assert.AreEqual(expectedStackSize, stack.Size());
+        }
+
+        [Test]
+        public void StackSizeShouldBeDecrementedWhenItemSubtracted()
+        {
+            var stack = new MyStack();
+            stack.Push(TEST_ELEMENT);
+            stack.Pop();
+
+            Assert.Zero(stack.Size());
+        }
+
+        [Test]
+        public void StackContentShouldBeRemoved()
+        {
+            var stack = new MyStack();
+            stack.Push(TEST_ELEMENT);
+            stack.Clear();
+
+            Assert.Zero(stack.Size());
+        }
+
+        [Test]
+        public void StackIsEmptyReturnTrueWhenNoItemsAdded()
+        {
+            var stack = new MyStack();
+            Assert.True(stack.IsEmpty()); 
+        }
+
+        [Test]
+        public void StackIsEmptyReturnFalseWhenItemsAdded()
+        {
+            var stack = new MyStack();
+            stack.Push(TEST_ELEMENT);
+
+            Assert.False(stack.IsEmpty());
+        }
+    }
+}
