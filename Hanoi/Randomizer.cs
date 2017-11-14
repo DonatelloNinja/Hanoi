61f0fb76c3a289c55686e32190bd2bf00586c4a4
Donny <donatelloninja@o2.pl>
2017-11-14 22:46:18 +0100

    TowerContainer implemented. Added class Randomizer

---
+++ Hanoi/Randomizer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hanoi
+{
+    public class Randomizer
+    {
+        public Randomizer()
+        {
+        }
+
+        //public static (TowerContainer towers, int numberOfElements)
+        //{
+
+        //}
+    }
+}
---
+++ Hanoi/TowerContainer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hanoi
+{
+    public class TowerContainer
+    {
+        private List<Tower> m_towers;
+
+        public TowerContainer()
+        {
+            m_towers = new List<Tower>();
+        }
+
+        public void Add(Tower tower)
+        {
+            m_towers.Add(tower);
+        }
+
+        public Tower GetTower(int index)
+        {
+            return m_towers[index];
+        }
+
+        public int Size()
+        {
+            return m_towers.Count();
+        }
+    }
+}
